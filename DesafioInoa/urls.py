5fafa1ad5621ac65d8b3d7c8a64b7491936be2d3
AdaltonF <adaltonfilho2003@gmail.com>
2025-01-10 19:55:05 -0300

    Add CRUD operations for "Ativo" and update home view
    Introduced functions to create, read, update, and delete "Ativo" objects, along with filtering and displaying related "Cotacao" data in the home view. Updated URLs to include new endpoints for these operations and reorganized URL structure for better clarity.

--- DesafioInoa/urls.py
+++ DesafioInoa/urls.py
@@ -7,9 +7,14 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('query/<str:ticker>', views.get_data),
-    path('', RedirectView.as_view(url='home/')),
-    path('home/', views.home),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
+    path('', RedirectView.as_view(url='home/')),
+    path('home/', views.home),
+    path('home/ativo/', views.ativo),
+    path('home/ativo/?id=<int:id>', views.ativo),
+    path('home/ativo/submit', views.submit_ativo),
+    path('home/ativo/delete/<int:id>', views.deletar_ativo),
+    path('home/historico', views.visualizar_historico)
 
 ]
--- core/views.py
+++ core/views.py
@@ -3,8 +3,8 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from core.forms import LoginForm, CreateAtivo
+from core.models import Ativo,Cotacao
 import requests
-# Create your views here.
 
 API_URL = 'https://brapi.dev/api/quote/'
 API_KEY = '6s4xhTzigCC6bbdpf6Pk3Z'
@@ -20,7 +20,9 @@ def get_data(request, ticker):
 
 def home(request):
     user = request.user
-    return render(request, 'model-page.html', {"user": user})
+    ativos = Ativo.objects.filter(usuario=user)
+    cotacoes = Cotacao.objects.filter(ativo__in=ativos)
+    return render(request, 'home.html', {"user": user, "ativos": ativos, "cotacoes": cotacoes})
 
 
 def login_user(request):
@@ -37,4 +39,70 @@ def submit_login(request):
         else:
             messages.error(request, "Login inválido! Tente novamente.")
             return redirect('/login/')
+    return redirect('/')
+
+
+# def visualizar_historico(request):
+#     # Recuperar todos os ativos para exibição no dropdown
+#     ativos = Ativo.objects.all()
+#     cotacoes = []
+#     selected_ativo = None
+#
+#     # Se o formulário foi enviado via POST
+#     if request.method == 'POST':
+#         selected_ativo = request.POST.get('selected_ativo')  # Recupera o ID do ativo selecionado
+#         if selected_ativo:  # Se um ativo válido foi selecionado
+#             cotacoes = Cotacao.objects.filter(ativo=selected_ativo)  # Filtra cotações pelo ativo selecionado
+#
+#     # Renderiza o template com os dados necessários
+#     return render(request, 'home.html', {
+#         'ativos': ativos,
+#         'cotacoes': cotacoes,
+#         'selected_ativo': selected_ativo,
+#     })
+
+def ativo(request):
+    id_ativo = request.GET.get('id')
+    print(id_ativo)
+    dado = {}
+
+    if id_ativo:
+        ativo = Ativo.objects.get(id=id_ativo)
+        dados_form = {'ticker': ativo.ticker,
+                    'limite_inferior': ativo.limite_inferior,
+                    'limite_superior': ativo.limite_superior,
+                    'periodicidade': ativo.periodicidade}
+        dados = {'form': CreateAtivo(initial=dados_form)}
+    else:
+        dados = {'form': CreateAtivo()}
+
+    return render(request, 'ativo.html', dados)
+
+def deletar_ativo(request, id):
+    usuario = request.user
+    ativo = Ativo.objects.get(id=id)
+
+    if ativo.usuario == usuario:
+        ativo.delete()
+    return redirect('/')
+
+
+def submit_ativo(request):
+    if request.POST:
+        ticker = request.POST.get('ticker')
+        lim_sup = request.POST.get('limite_superior')
+        lim_inf = request.POST.get('limite_inferior')
+        periodo = request.POST.get('periodicidade')
+        usuario = request.user
+        if Ativo.objects.filter(ticker__exact=ticker).exists():
+            Ativo.objects.filter(ticker__exact=ticker).update(ticker=ticker,
+                                                               limite_superior=lim_sup,
+                                                               limite_inferior=lim_inf,
+                                                               periodicidade=periodo)
+        else:
+            Ativo.objects.create(ticker=ticker,
+                               limite_superior=lim_sup,
+                               limite_inferior=lim_inf,
+                               periodicidade=periodo,
+                                usuario=usuario)
     return redirect('/')
